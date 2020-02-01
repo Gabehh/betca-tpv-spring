4daa29ff656a8824406a8b41af896efaec9f2954
jbernal <miw@etsisi.upm.es>
2020-02-01 20:59:26 +0100

    update api logs to webflux

--- src/main/java/es/upm/miw/betca_tpv_spring/ApiLogs.java
+++ src/main/java/es/upm/miw/betca_tpv_spring/ApiLogs.java
@@ -1,12 +1,17 @@
 package es.upm.miw.betca_tpv_spring;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
 
 @Profile("dev")
 @Component
@@ -24,32 +29,32 @@ public class ApiLogs {
                 .info("------------------------------- o -------------------------------");
         StringBuilder log = new StringBuilder(jp.getSignature().getName() + " >>>");
         for (Object arg : jp.getArgs()) {
-            log.append("\n   ARG: ").append(arg);
+            log.append("; ARG: ").append(arg);
         }
         LogManager.getLogger(jp.getSignature().getDeclaringTypeName()).info(log);
     }
 
-    @AfterReturning(pointcut = "allResources()", returning = "result")
-    public void apiResponseLog(JoinPoint jp, Object result) {
-        ObjectMapper mapper = new ObjectMapper();
-        String resultAsString;
-        try {
-            resultAsString = mapper.writeValueAsString(result);
-        } catch (JsonProcessingException e) {
-            resultAsString = result.toString();
+    @AfterReturning(pointcut = "allResources()", returning = "returnValue")
+    public void apiResponseLog(JoinPoint jp, Object returnValue) {
+        Consumer<Object> consumer = result -> {
+            String log = "<<< Return << " + jp.getSignature().getName() + ": " + result.toString();
+            if (log.length() > 1000) {
+                log = log.substring(0, 1000) + ".... (+" + log.length() + " characters)";
+            }
+            LogManager.getLogger(jp.getSignature().getDeclaringTypeName()).debug(log);
+        };
+        Consumer<Throwable> error = throwable -> {
+            String log = "<<< Return Exception << " + jp.getSignature().getName() + ": " + throwable.toString();
+            LogManager.getLogger(jp.getSignature().getDeclaringTypeName()).debug(log);
+        };
+        String result = returnValue.getClass().getSimpleName().substring(0, 4);
+        if ("Flux".equals(result)) {
+            ((Flux<Object>) returnValue).subscribe(consumer, error);
+        } else if ("Mono".equals(result)) {
+            ((Mono<Object>) returnValue).subscribe(consumer, error);
+        } else {
+            LogManager.getLogger(jp.getSignature().getDeclaringTypeName()).debug("???: " + returnValue.toString());
         }
-        String log = "<<< Return << " + jp.getSignature().getName() + ": " + resultAsString;
-        if (log.length() > 1000) {
-            log = log.substring(0, 1000) + ".... (+" + log.length() + " characters)";
-        }
-        LogManager.getLogger(jp.getSignature().getDeclaringTypeName()).info(log);
-    }
-
-    @AfterThrowing(pointcut = "allResources()", throwing = "exception")
-    public void apiResponseExceptionLog(JoinPoint jp, Exception exception) {
-        String log = "<<< Return Exception << " + jp.getSignature().getName() + ": " + exception.getClass()
-                .getSimpleName() + "->" + exception.getMessage();
-        LogManager.getLogger(jp.getSignature().getDeclaringTypeName()).info(log);
     }
 
 }
