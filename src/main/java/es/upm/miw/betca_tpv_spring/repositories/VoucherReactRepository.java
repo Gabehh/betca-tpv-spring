8136e97a09fce8fe4c8a414c295aa23e63a1f00a
jesusgarce <tarraga.jesus@gmail.com>
2020-03-06 18:36:39 +0100

    Add search Voucher service #5

--- src/main/java/es/upm/miw/betca_tpv_spring/repositories/VoucherReactRepository.java
+++ src/main/java/es/upm/miw/betca_tpv_spring/repositories/VoucherReactRepository.java
@@ -1,8 +1,18 @@
 package es.upm.miw.betca_tpv_spring.repositories;
 
 import es.upm.miw.betca_tpv_spring.documents.Voucher;
+import org.apache.tomcat.jni.Local;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import reactor.core.publisher.Flux;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
 
 public interface VoucherReactRepository extends ReactiveSortingRepository<Voucher, String> {
 
+    Flux<Voucher> findByIdAndCreationDateBetween(String id, LocalDateTime firstDate, LocalDateTime finalDate);
+
+    Flux<Voucher> findAllByCreationDateBetween(LocalDateTime firstDate, LocalDateTime finalDate);
+
+    Flux<Voucher> findAllByCreationDateIsBefore(LocalDateTime finalDate);
 }
